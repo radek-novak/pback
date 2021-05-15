afe56bed17129d1ade3165104136225f2c82b17f
Radek Novak <radek.novak.cze@gmail.com>
2021-05-15 17:52:25 +0200

    timeout

--- src/lib.ts
+++ src/lib.ts
@@ -1,7 +1,20 @@
+const rejectAfter = (ms: number) =>
+  new Promise<never>((_resolve, reject) => {
+    setTimeout(() => {
+      reject("Timeout passed");
+    }, ms);
+  });
+
+const defaultOptions = {
+  timeout: 10_000,
+};
+
 const deduper = <Key extends any, FnParams extends any[], Result extends any>(
   keyFn: (...params: FnParams) => Key,
-  fn: (...params: FnParams) => Promise<Result>
+  fn: (...params: FnParams) => Promise<Result>,
+  options = defaultOptions
 ) => {
+  const currentOptions = { ...defaultOptions, ...options };
   const cache = new Map<Key, Promise<Result>>();
 
   return (...params: FnParams) => {
@@ -18,12 +31,14 @@ const deduper = <Key extends any, FnParams extends any[], Result extends any>(
 
     // Record and return new calculation
     // from here all the calls to the same
-    const newP = fn(...params).then((res) => {
+    const newP = Promise.race([
+      fn(...params),
+      rejectAfter(currentOptions.timeout),
+    ]).finally(() => {
       cache.delete(key);
-      return res;
     });
 
-    cache.set(key, newP);
+    cache.set(key, newP as Promise<Result>);
 
     return newP;
   };
--- src/test.ts
+++ src/test.ts
@@ -1,6 +1,11 @@
 import { deduper } from "./lib";
 
-async function run() {
+const sleep = (ms: number) =>
+  new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+
+async function succeeding() {
   let calledTimes = 0;
 
   const expensiveCalculation = (id: string, time = 1000) => {
@@ -27,4 +32,67 @@ async function run() {
   );
 }
 
-run();
+async function timingOut() {
+  let calledTimes = 0;
+
+  const expensiveCalculation = (id: string, time = 6000) => {
+    calledTimes++;
+
+    return new Promise((resolve) => {
+      setTimeout(() => resolve(id.toUpperCase()), time);
+    });
+  };
+
+  const dedupedExpensiveCalc = deduper((id, n) => id, expensiveCalculation);
+
+  const result = await Promise.all(
+    new Array(100)
+      .fill(0)
+      .map((_, i) => dedupedExpensiveCalc(i % 3 === 0 ? "a" : "b", 6000))
+  ).catch((e) => e);
+
+  console.log(result);
+
+  console.assert(result === "Timeout passed", `Timed out: ${result}`);
+  console.assert(
+    calledTimes === 2,
+    `Was called ${calledTimes} times instead of twice`
+  );
+}
+
+async function succeedingLastLate() {
+  let calledTimes = 0;
+
+  const expensiveCalculation = (id: string, time = 100) => {
+    calledTimes++;
+
+    return new Promise<string>((resolve) => {
+      setTimeout(() => resolve(id.toUpperCase()), time);
+    });
+  };
+
+  const dedupedExpensiveCalc = deduper((id, n) => id, expensiveCalculation, {
+    timeout: 1000,
+  });
+
+  dedupedExpensiveCalc("a", 10);
+  dedupedExpensiveCalc("b", 10);
+  dedupedExpensiveCalc("c", 10);
+  dedupedExpensiveCalc("a", 10);
+  await sleep(1);
+  dedupedExpensiveCalc("a", 10);
+  await sleep(2000);
+  dedupedExpensiveCalc("a", 10);
+
+  await sleep(100);
+
+  console.log(calledTimes);
+
+  console.assert(
+    calledTimes === 4,
+    `Was called ${calledTimes} times instead of 4`
+  );
+}
+
+const scenarios = [succeeding, timingOut, succeedingLastLate];
+scenarios.forEach(async (sc) => await sc());
